734d5c441a427578d524168672173ca39856deb3
shofikheiro <shofiyyahumairo92@gmail.com>
2025-08-11 16:34:55 +0700

    fixed: login and signup password input toggle visibility. Add: Searchbar homepage

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+function toggleVisibility(){
+    const visibility = document.querySelector('.visibility')
+    const password = document.querySelector('.password')
+    if(password.getAttribute('type') === "password"){
+        password.setAttribute('type', 'text');
+        visibility.setAttribute('src', "Asset/visibility.svg"); 
+    }else{
+        password.setAttribute('type', 'password');
+        visibility.setAttribute('src', "Asset/visibility_off.svg");
+    }
+}
+
+function openSearchBar(){
+    const start = document.getElementById('mulaiCari')
+    const open = document.getElementById('searchBar')
+    start.style.display = 'none';
+    open.style.display = 'flex';
+}
